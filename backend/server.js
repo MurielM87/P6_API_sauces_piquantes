6b7985a5b4f3d950655d5be52cbe006e3abc8187
Muriel MARCHAL <muriel.marchal@hotmail.com>
2022-04-05 16:48:02 +0200

    modifications app.js & server.js

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
 const http = require('http');
+const app = require('./app');
 
-const server = http.createServer((req, res) => {
-    res.end('voila la reponse du server') //le serveur est prêt
-});
+app.set('port', process.env.PORT || 3000);
+const server = http.createServer(app);
 
 //le serveur ecoute les requetes envoyees
 server.listen(process.env.PORT || 3000); //port 3000 par defaut
