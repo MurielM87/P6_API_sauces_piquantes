927217349c625a430488724436c073a85eede715
Muriel MARCHAL <muriel.marchal@hotmail.com>
2022-04-09 15:31:25 +0200

    creation user & password

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -17,5 +17,22 @@ exports.signup = (req, res, next) => {
 };
 
 exports.login = (req, res, next) => {
-
+    User.findOne({email: req.body.email})
+    .then(user => {
+        if (!user) {
+            return res.status(401).json({error : 'utilisateur inconnu'});
+        }
+        bcrypt.compare(req.body.password, user.password)
+            .then(valid => {
+                if(!valid) {
+                    return res.status(401).json({error : 'mot de passe incorrect'});
+                }
+                res.status(200).json({
+                    userId: user._id,
+                    token: 'TOKEN'
+                });
+            })
+            .catch(error => res.status(500).json({error}));
+    })
+    .catch(error => res.status(500).json({error}));
 };
