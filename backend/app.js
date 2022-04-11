7d5072648dd7e226303aaad6989c16b058341ab0
Muriel MARCHAL <muriel.marchal@hotmail.com>
2022-04-11 17:19:34 +0200

    installation cors

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,19 @@ const path = require('path');
 const stuffRoutes = require('./routes/stuff');
 const userRoutes = require('./routes/user');
 
+/*
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://MurielM87:<password>@cluster0.2q0qu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+*/
+/*
+"mongodb+srv://cluster0.2q0qu.mongodb.net/myFirstDatabase", //--apiVersion 1 --username MurielM87
+*/
 mongoose.connect(`mongodb://localhost:27017/node-api`,
   { useNewUrlParser: true,
     useUnifiedTopology: true })
--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -3,6 +3,7 @@ const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
 
+//SIGN UP
 exports.signup = (req, res, next) => {
     bcrypt.hash(req.body.password, 10)
     .then(hash => {
@@ -17,6 +18,7 @@ exports.signup = (req, res, next) => {
     .catch(error => res.status(500).json({error})); //erreur serveur
 };
 
+//LOG IN
 exports.login = (req, res, next) => {
     User.findOne({email: req.body.email})
     .then(user => {
