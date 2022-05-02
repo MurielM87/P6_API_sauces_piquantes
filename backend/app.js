d48a52bbc673c3a9724e75fc222e468118eab573
Muriel MARCHAL <muriel.marchal@hotmail.com>
2022-05-02 10:13:07 +0200

    modification gitignore

--- backend/app.js
+++ backend/app.js
@@ -12,8 +12,8 @@ const userRoutes = require('./routes/user');
 mongoose.connect(`mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_CLUSTER_NAME}.mongodb.net/${process.env.MONGODB_DATABASE_NAME}?retryWrites=true&w=majority`,
   { useNewUrlParser: true,
     useUnifiedTopology: true })
-  .then(() => console.log('Connexion à MongoDB réussie !'))
-  .catch(() => console.log('Connexion à MongoDB échouée !'));
+  .then(() => console.log('MongoDB connnected !'))
+  .catch(() => console.log('MongoDB not connected !'));
 
 const app = express();
 
