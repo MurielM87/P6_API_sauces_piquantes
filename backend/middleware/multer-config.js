2c750655179c8f1a8958829fe9cbc42ecd4bba99
Muriel MARCHAL <muriel.marchal@hotmail.com>
2022-05-09 17:03:40 +0200

    multer + gitignore

--- backend/middleware/multer-config.js
+++ backend/middleware/multer-config.js
@@ -1,21 +1,45 @@
+const res = require('express/lib/response');
 const multer = require('multer');
 
 //formats des images acceptees
 const MIME_TYPES = {
     'image/jpg': 'jpg',
     'image/jpeg': 'jpg',
-    'image/png': 'png'
+    'image/png': 'png',
 };
 
+//definition du lieu de stockage
 const storage = multer.diskStorage({
-    destination:(req, file, callback) => {
+    destination: (req, file, callback) => {
         callback(null, 'images')
     },
+    //definition du nom du fichier image
     filename: (req, file, callback) => {
-        const name = file.originalname.split(' ').join('_');
-        const extension = MIME_TYPES[file.mimetype];
-        callback(null, name + Date.now() + '.' + extension);
-    }
+       
+        //etape 0 pour verifier si l'extension dans les extensions autorisees
+
+        if (file.mimetype in MIME_TYPES) {
+            const extension = MIME_TYPES[file.mimetype];
+
+            //etape 1 pour retirer l'extension du nom du fichier
+            let name = file.originalname.split('.');
+            name = name[0];
+
+            //etape 2 pour sanitariser le nom du fichier
+            name = name.split(' ').join('_');
+
+            console.log("mimetype ok");
+            callback(null, name + Date.now() + '.' + extension);
+            
+        } else {
+            console.log("mimetype ko")
+            callback(false, '');
+            return false;
+        }
+        
+    },
+
+
 });
 
 module.exports = multer({ storage }).single('image');
